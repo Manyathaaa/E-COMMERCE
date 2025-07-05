a6304b65f22c8fe103ce7ed912355a959ecbb603
Manyathaaa <mailtomanyatha@gmail.com>
2025-07-05 11:27:31 +0530

    database connection successfull

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
+//env config
 dotenv.config();
 
+//database config
+connectDB();
+
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
 //for api
 
 app.get("/", (req, res) => {
@@ -11,8 +22,16 @@ app.get("/", (req, res) => {
 });
 
 //port
-const PORT = process.env.PORT || 1010;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
+// import express from "express";
+// const app = express();
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
