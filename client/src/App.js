2f95ce1d36073236a1280c27494e38ea29928867
Manyathaaa <mailtomanyatha@gmail.com>
2025-07-06 16:57:13 +0530

    react app components

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Layout from "./components/Layout/Layout.js";
+
+function App() {
+  return (
+    <Layout>
+      <h1>Ecommerce App</h1>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout = (props) => {
+  //props is an object that contains all the properties passed to the component
+  // This component serves as a layout wrapper for the application
+  // It can be used to include common elements like headers, footers, or sidebars
+  return (
+    <div>
+      <Header />
+      <main>{props.children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
+
+//props are mainly used to pass data from a parent component to a child component
