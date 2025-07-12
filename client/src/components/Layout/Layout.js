a623ccd750a00bdf9be6a77f62003c608cb3afa8
Manyathaaa <mailtomanyatha@gmail.com>
2025-07-12 20:18:22 +0530

    SEO created successfully

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Helmet } from "react-helmet";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   //props is an object that contains all the properties passed to the component
   // This component serves as a layout wrapper for the application
   // It can be used to include common elements like headers, footers, or sidebars
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
       <Header />
       <main style={{ minHeight: "76vh" }}>{children}</main>
       <Footer />
