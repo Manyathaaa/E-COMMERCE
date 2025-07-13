d4a3f00203e8d7bca5c3afb464317dc177d5f610
Manyathaaa <mailtomanyatha@gmail.com>
2025-07-13 14:51:58 +0530

    login done successfully

---
+++ client/src/pages/auth/login.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // form submit function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/login`,
+        { email, password },
+        { withCredentials: true }
+      );
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  console.log(
+    "logging into:",
+    `${process.env.REACT_APP_API}/api/v1/auth/login`
+  );
+
+  return (
+    <Layout title="login - Ecommerce app">
+      <div className="register">
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-center mb-4">LOGIN</h2>
+          <div className="mb-3">
+            <label htmlFor="email">EMAIL</label>
+            <input
+              type="email"
+              id="email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">PASSWORD</label>
+            <input
+              type="password"
+              id="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="checkLogin"
+            />
+            <label className="form-check-label" htmlFor="checkLogin">
+              Remember me
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary w-100">
+            Login
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
