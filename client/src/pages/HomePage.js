4e3bf8d77428d52ae64617fb9698101ecd5de397
Manyathaaa <mailtomanyatha@gmail.com>
2025-07-26 21:54:51 +0530

    Implement product fetching and layout updates in HomePage component

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import Layout from "../components/Layout/Layout";
 import { useAuth } from "../context/auth";
+import axios from "axios";
 
 const HomePage = () => {
-  const [auth] = useAuth();
+  const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = [];
+
+  //get products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/products/get-product");
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
   return (
-    <Layout>
-      <h1>HomePage</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    <Layout title={"All Products - Best Offer"}>
+      <div className="row mt-3">
+        <div className="col-md-3">
+          <h4 className="text-center">Filter By Category </h4>
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center">All Products</h1>
+          <div className="d-flex flex-wrap">Products</div>
+        </div>
+      </div>
     </Layout>
   );
 };
