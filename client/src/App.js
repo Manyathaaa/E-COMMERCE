5255ea4d6f054b56cc8199e0fafe058f78c0a80a
Manyathaaa <mailtomanyatha@gmail.com>
2025-08-16 22:34:14 +0530

    feat: add Navbar component and update routing in App.js; enhance styles in index.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import PageNotFound from "./pages/PageNotFound";
@@ -20,40 +20,66 @@ import Users from "./pages/admin/user";
 import Orders from "./pages/user/orders";
 import Profile from "./pages/user/profile";
 import UpdateProduct from "./pages/admin/UpdateProduct";
+import CategoryPage from "./pages/CategoryPage";
 function App() {
   return (
     <>
+      <Navbar />
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/policy" element={<Policy />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/categories" element={<CategoryPage />} />
+          {/* Private User Route */}
+          import CategoryPage from "./pages/CategoryPage";
+          <Route path="/user" element={<PrivateRoute />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="user/orders" element={<Orders />} />
+            <Route path="user/profile" element={<Profile />} />
+          </Route>
+          {/* Private Admin Route */}
+          <Route path="/admin" element={<AdminRoute />}>
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="create-category" element={<CreateCategory />} />
+            <Route path="create-product" element={<CreateProduct />} />
+            <Route path="product/:slug" element={<UpdateProduct />} />
+            <Route path="product" element={<Products />} />
+            <Route path="user" element={<Users />} />
+          </Route>
+          {/* 404 Route */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
       <ToastContainer />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/policy" element={<Policy />} />
-
-        {/* Private User Route */}
-        <Route path="/user" element={<PrivateRoute />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="user/orders" element={<Orders />} />*
-          <Route path="user/profile" element={<Profile />} />
-        </Route>
-
-        {/* Private Admin Route */}
-        <Route path="/admin" element={<AdminRoute />}>
-          <Route path="dashboard" element={<AdminDashboard />} />
-          <Route path="create-category" element={<CreateCategory />} />
-          <Route path="create-product" element={<CreateProduct />} />
-          <Route path="product/:slug" element={<UpdateProduct />} />
-          <Route path="product" element={<Products />} />
-          <Route path="user" element={<Users />} />
-        </Route>
-
-        {/* 404 Route */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
     </>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Bolt.New</div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/policy">Policy</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
