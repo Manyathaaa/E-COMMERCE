fd3630a188dbc51c427e3a5978fb6b4d4aa00b3b
Manyathaaa <mailtomanyatha@gmail.com>
2025-08-18 14:53:34 +0530

    refactor: remove Header and ToastContainer from Layout component for cleaner structure

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Footer from "./Footer";
-import Header from "./Header";
 import { Helmet } from "react-helmet";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const Layout = ({ children, title, description, keywords, author }) => {
   //props is an object that contains all the properties passed to the component
@@ -18,11 +15,7 @@ const Layout = ({ children, title, description, keywords, author }) => {
         <meta name="author" content={author} />
         <title>{title}</title>
       </Helmet>
-      <Header />
-      <main style={{ minHeight: "76vh" }}>
-        <ToastContainer />
-        {children}
-      </main>
+      <main style={{ minHeight: "76vh" }}>{children}</main>
       <Footer />
     </div>
   );
